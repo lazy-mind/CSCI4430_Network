6ca284025870e83503662df8c5f01ec9b6c1a3ff
Lazy_guy <michael251902523@gmail.com>
2017-04-03 00:58:58 +0800

    project skeleton code

--- Project_1_Skeleton/client/mtcp_client.c
+++ Project_1_Skeleton/client/mtcp_client.c
@@ -9,10 +9,13 @@
 #include "mtcp_client.h"
 
 /* -------------------- Global Variables -------------------- */
+//global variable for making connection
 int sockfd;
 struct sockaddr_in *addr;
 struct sockaddr_in server;
 
+//global variable for determine which state we are in
+typedef enum {INIT,HS3,RW,HS4,END,NIL} state_t;	
 state_t state = NIL;
 unsigned int current_ack;
 unsigned int last_seq;
@@ -20,10 +23,71 @@ unsigned char last_recv_type=-1;
 unsigned char last_sent_type=-1;
 ssize_t sendto_err = 0;
 
-
-buffer_t *sendbuf;
-
+//global variable for the sending buffer, which is a common thing
 static pthread_mutex_t buffer_mutex = PTHREAD_MUTEX_INITIALIZER;
+#define MAX_BUF_SIZE 1024
+#define SEND_BUF_SIZE 268435456
+typedef struct
+{
+	int front,rear;
+	int capacity;
+	unsigned char *array;
+}buffer_t;
+//the global sendbuf
+buffer_t *sendbuf=(buffer_t *)malloc(sizeof(buffer_t));
+sendbuf->capacity = size;
+sendbuf->front = -1;
+sendbuf->rear = -1;
+sendbuf->array=(unsigned char *)malloc(sendbuf->capacity*sizeof(unsigned char));
+//function for making change to the buffer
+int is_empty(buffer_t *q)
+{
+	return (q->front==-1);
+}
+
+int is_full(buffer_t *q)
+{
+	return (((q->rear+1)%q->capacity) == q->rear);
+}
+
+int buf_size(buffer_t *q)
+{
+	if(q->front==-1)return 0;
+	return (q->rear - q->front +1)%q->capacity;
+}
+	
+int enqueue(buffer_t *q, unsigned char *src,  int len)
+{	
+	if(DEBUG)printf("buf_size=%d\n",buf_size(q));
+	if(is_full(q))return 0;
+	else if(len > (q->capacity-buf_size(q)))return 0;
+	else
+	{
+		int tmp = buf_size(q);
+		q->rear = (q->rear + len)%q->capacity;
+		memcpy(&(q->array[q->rear-len+1]),src,len);
+		if(q->front==-1)
+		{
+			q->front = q->rear - len+1;
+		}
+		return buf_size(q)-tmp;
+	}
+}
+
+int dequeue(buffer_t *q,unsigned char *dst, int len)
+{
+	if(is_empty(q))return 0;
+	else
+	{
+		memcpy(dst,&(q->array[q->front]),len);
+		if(q->front==(q->rear - len+1))
+			q->front=q->rear=-1;
+		else
+			q->front=(q->front + len)%q->capacity;
+		return 1;
+	}
+}
+
 
 /* ThreadID for Sending Thread and Receiving Thread */
 static pthread_t send_thread_pid;
@@ -41,7 +105,7 @@ static pthread_mutex_t info_mutex = PTHREAD_MUTEX_INITIALIZER;
 void mtcp_connect(int socket_fd, struct sockaddr_in *server_addr){
 
 	//create a common used buffer
-	//TBD
+	//which is the sendbuf, created
 
 	//initialize the address for sending thread to use
 	current_ack=0;//ack
@@ -54,12 +118,12 @@ void mtcp_connect(int socket_fd, struct sockaddr_in *server_addr){
     pthread_create(&recv_thread_pid,NULL,receive_thread,NULL);
 
     //wake the sending thread to get the 3-way-handshake
-    //TBD : what state is it
+    state=HS3;
     pthread_cond_signal(&send_thread_sig);
     pthread_cond_wait(&app_thread_sig,&app_thread_sig_mutex);
 
     //change the state to transmission
-    //what state is it
+    state=RW;
 
     return;
 }
@@ -69,12 +133,17 @@ int mtcp_write(int socket_fd, unsigned char *buf, int buf_len){
 	//check if i am in the ending state
 	//TBD : if I am in the ending state, don't send anymore, return -1
 	int cond=3;
+	if(sendto_err==-1 || state==HS4 || state==NIL || state==END){cond=1;}
+	else if(if_full(buffer)){cond=2;}
+
 	if(cond==1){
+		printf("the connection is ended/ going to end\n");
 		return -1;
 	}
     //keep writing to the maximum buffer area
     //if buffer is full, don't send anymore, return 0
 	else if(cond==2){
+		printf("the buffer is full.\n");
 		return 0;
 	}
 
@@ -91,12 +160,20 @@ int mtcp_write(int socket_fd, unsigned char *buf, int buf_len){
 void mtcp_close(int socket_fd){
     //wake the sending thread to send signal to finish the 4-way-handshake
    	//wait for the buffer to send out, and ack is recieved
-   	int ack_num=1;
-   	while(ack_num==end_ack_num){
-   		pthread_mutex_lock(&info_mutex);
-		ack_num=update_ack_num(); // TBD : to be implemented
+   	unsigned char flag = 1;
+	while(flag)	//block until all buffer sent
+	{
+		pthread_mutex_lock(&sendbuf_mutex);
+		flag = !is_empty(sendbuf);
+		pthread_mutex_unlock(&sendbuf_mutex);
+	}
+	flag = 1;
+	while(flag)	//block until ACK of last sent DATA recieved
+	{
+		pthread_mutex_lock(&info_mutex);
+		if(last_seq!=current_ack) flag = 0;
 		pthread_mutex_unlock(&info_mutex);
-   	}
+	}
     //TBD : change the state to end
 
    	//wake the send thread for 4-way-handshake
--- Project_1_Skeleton/server/mtcp_server.c
+++ Project_1_Skeleton/server/mtcp_server.c
@@ -43,12 +43,13 @@ void mtcp_accept(int socket_fd, struct sockaddr_in *server_addr){
 	addr=(struct sockaddr_in)malloc(sizeof(*addr));
 	addr = client_addr;
 	//change the state to accpet 3-way-handshake
+	printf("I am changing the state to 3-way-handshake");
 
 	//create the thread:
 	pthread_create(&send_thread_pid,NULL,send_thread,NULL)
 	pthread_create(&recv_thread_pid,NULL,receive_thread,NULL)
 	
-	//wait for the 4-way-handshake to complete
+	//wait for the 3-way-handshake to complete
 	pthread_mutex_lock(&app_thread_sig_mutex);
 	pthread_cond_wait(&app_thread_sig,&app_thread_sig_mutex);
 	pthread_mutex_unlock(&app_thread_sig_mutex);
@@ -60,15 +61,18 @@ int mtcp_read(int socket_fd, unsigned char *buf, int buf_len){
 	int cond=3;
 	if(cond==1){
 		//the connection is end, while the main program still try to read
+		printf("the connection is already ended!");
 		return -1;
 	}
 	else if(cond==2){
 		//the recieve buffer is empty, while the program still want to read
 		//in this case we wait for the 
+		printf("the recieve buffer is empty!");
 		return 
 	}
 	else if(cond==3){
 		//the buffer has data, then we perform the read function
+		printf("now we are reading!");
 	}
 	return 1;
 }
